371af754ca95a4e742fedd4568749856ac4fc908
Tyler Eades <tyler@mac-mini.attlocal.net>
2024-07-22 09:36:55 -0500

    add lazy vim

--- .config/nvim/lua/tyler/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "tyler.lsp.mason"
-require("tyler.lsp.handlers").setup()
-require "tyler.lsp.null-ls"
