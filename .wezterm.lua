d68354394c1d6e30398341ac57afcb2bf6ade1ac
tylerea <tylerea@zillowgroup.com>
2025-12-03 21:39:31 -0600

    updates

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,8 +8,9 @@ local config = wezterm.config_builder()
 config.default_cwd = "$HOME"
 
 -- For example, changing the color scheme:
-config.color_scheme = "Tokyo Night Moon"
+-- config.color_scheme = "Tokyo Night Moon"
 -- config.color_scheme = "Tokyo Night Day"
+config.color_scheme = "Catppuccin Mocha"
 
 config.font = wezterm.font("Iosevka Nerd Font Mono")
 config.font_size = 16
@@ -19,6 +20,11 @@ config.window_padding = {
 	right = "1cell",
 	top = "1cell",
 	bottom = "1cell",
+}
+
+-- and finally, return the configuration to wezterm
+config.keys = {
+  {key="Enter", mods="SHIFT", action=wezterm.action{SendString="\x1b\r"}},
 }
 
 -- and finally, return the configuration to wezterm
